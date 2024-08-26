110028fbc75fb5d50fd1ca5e4b9fcee54d33578f
ShevtsovTN <timur.shevtsov1985@gmail.com>
2024-08-26 08:12:42 +0200

    Fix DemoUserController.php

--- modules/Auth/Http/Controllers/DemoUserController.php
+++ modules/Auth/Http/Controllers/DemoUserController.php
@@ -2,18 +2,15 @@
 
 namespace Modules\Auth\Http\Controllers;
 
-use App\Contracts\ServiceRepositoryContract;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Modules\Auth\Dto\LoginDTO;
 use Modules\Auth\Dto\RegisterDTO;
-use Modules\Auth\Strategies\AuthHandlerStrategy;
 
 class DemoUserController extends Controller
 {
     public function __construct(
-        private readonly ServiceRepositoryContract $serviceRepository,
-        private readonly AuthHandlerStrategy $authHandlerStrategy
+        private readonly DemoUserAuthService $service
     )
     {
     }
@@ -21,24 +18,17 @@ class DemoUserController extends Controller
     public function login(DemoUserLoginRequest $request): JsonResource
     {
         $dto = LoginDTO::fromRequest($request->validated());
-        $serviceDto = $this->serviceRepository->getOne($dto->serviceUuid);
 
-        $handler = $this->authHandlerStrategy->getHandler($serviceDto);
+        $user = $this->service->login($dto);
 
-        $user = $handler->getService()->login($dto);
-
-        return $handler->getResource()->make($user);
+        return DemoUserResource::make($user);
     }
 
     public function register(DemoUserRegisterRequest $request): JsonResource
     {
         $dto = RegisterDTO::fromRequest($request->validated());
-        $serviceDto = $this->serviceRepository->getOne($dto->serviceUuid);
+        $user = $this->service->register($dto);
 
-        $handler = $this->authHandlerStrategy->getHandler($serviceDto);
-
-        $user = $handler->getService()->register($dto);
-
-        return $handler->getResource()->make($user);
+        return DemoUserResource::make($user);
     }
 }
